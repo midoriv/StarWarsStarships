bb8f3329709b3b0b62b08bc2f50f452f501e8608
Midori Verdouw <s3575912@student.rmit.edu.au>
2022-04-08 22:44:42 +1000

    Move `Starship` and `StarshipPage` definitions into a separate file

--- StarWarsStarships/ContentViewModel.swift
+++ StarWarsStarships/ContentViewModel.swift
@@ -50,30 +50,4 @@ class ContentViewModel: ObservableObject {
     }
 }
 
-struct StarshipPage: Codable {
-    var count: Int
-    var next: String?
-    var results: [Starship]
-}
-
-struct Starship: Hashable, Codable {
-    var name: String
-    var model: String
-    var manufacturer: String
-    var cost_in_credits: String
-    var length: String
-    var max_atmosphering_speed: String
-    var crew: String
-    var passengers: String
-    var cargo_capacity: String
-    var consumables: String
-    var hyperdrive_rating: String
-    var MGLT: String
-    var starship_class: String
-    var pilots: [String]
-    var films: [String]
-    var created: String
-    var edited: String
-    var url: String
-}
 
