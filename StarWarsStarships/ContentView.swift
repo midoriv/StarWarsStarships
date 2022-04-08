1c7049a4f6e0e1d43560baa13c45db63b3b9a9ca
Midori Verdouw <s3575912@student.rmit.edu.au>
2022-04-08 15:45:43 +1000

    Initial Commit

---
+++ StarWarsStarships/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StarWarsStarships
+//
+//  Created by Midori Verdouw on 8/4/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StarWarsStarships/StarWarsStarshipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsStarshipsApp.swift
+//  StarWarsStarships
+//
+//  Created by Midori Verdouw on 8/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsStarshipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
