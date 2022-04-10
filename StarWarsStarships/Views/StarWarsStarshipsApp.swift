6eb0e78528dc16efbfe571f64f96b7ab3a072743
Midori Verdouw <s3575912@student.rmit.edu.au>
2022-04-10 23:58:11 +1000

    Add navigation view style for iPad

--- StarWarsStarships/Views/StarWarsStarshipsApp.swift
+++ StarWarsStarships/Views/StarWarsStarshipsApp.swift
@@ -17,6 +17,7 @@ struct StarWarsStarshipsApp: App {
                 StarshipsListView()
                     .environmentObject(viewModel)
             }
+            .navigationViewStyle(StackNavigationViewStyle())
         }
     }
 }
